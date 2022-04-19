aceaec4cc8132e504b6288546a3a39618c61da77
SukheeChoi <95064440+SukheeChoi@users.noreply.github.com>
2022-04-19 09:11:54 +0900

    파일 다운로드, 예외 처리, Spring Tag Library, Bean Name View Resolver

--- src/main/java/com/mycompany/webapp/controller/Ch09Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch09Controller.java
@@ -1,11 +1,17 @@
 package com.mycompany.webapp.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.net.URLEncoder;
 import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -73,5 +79,36 @@ public class Ch09Controller {
 		String json = jsonObject.toString();
 		
 		return json;
+	}
+	
+	@RequestMapping("/filedownload")
+	public void filedownload(int fileNo, HttpServletResponse response
+			, @RequestHeader("User-Agent") String userAgent) throws Exception {//void: 직접 응답을 만들것.
+		//DB에서 가져올 정보
+		String contentType = "image/jpeg";
+		String originalFilename = "목록표.jpeg";
+		String saveFilename = "1650226124326-목록표.jpeg";
+		
+		//응답 내용의 데이터 타입을 응답 헤더에 추가
+		response.setContentType(contentType);
+		
+		//다운로드할 파일명을 헤더에 추가
+		if(userAgent.contains("Trident") || userAgent.contains("MSIE")) {
+			//IE 브라우저일 경우
+			originalFilename = URLEncoder.encode(originalFilename, "UTF-8");
+		} else {
+			//크롬, 엣지, 사파리일 경우
+			originalFilename = new String(originalFilename.getBytes("UTF-8"), "ISO-8859-1");
+		}
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + originalFilename + "\"");
+		
+		//파일 데이터를 응답 본문에 싣기
+		File file = new File("/Users/choisukhee/Osstem/uploadfiles/" + saveFilename);
+		if(file.exists()) {
+			FileCopyUtils.copy(
+					new FileInputStream(file)
+					, response.getOutputStream()
+			);
+		}
 	}
 }
